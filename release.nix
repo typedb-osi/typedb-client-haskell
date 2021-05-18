951bebb914e02574a14cf78cb6cfc6431babbeba
faebl <faebl.taylor@protonmail.com>
2021-05-18 20:58:29 +0200

    updated build for new grpc build interface

--- release.nix
+++ release.nix
@@ -1,7 +1,9 @@
 let
     config = {allowBroken = true; allowUnfree = true;};
     nixpkgs = import ./nixpkgs.nix { inherit config; };
-    grpcPkg = (import ./gRPC-haskell/release.nix).awakePkgs;
+    overlays = [(import ./gRPC-haskell/release.nix).overlay ];
+    awakePkgs = import ./nixpkgs.nix { inherit config overlays; };
+    grpcPkg = awakePkgs;
     grakn-haskell = grpcPkg.haskellPackages.callPackage ./default.nix {};
 in {
     build = grpcPkg.usesGRPC grakn-haskell;
