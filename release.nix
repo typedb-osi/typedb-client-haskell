c23d3a8c6eb3223fd020144c5155013ae33dbfad
faebl <faebl.taylor@protonmail.com>
2021-06-27 20:30:00 +0200

    moved submodule dependency to nix build plan

--- release.nix
+++ release.nix
@@ -1,12 +1,17 @@
 let
     config = {allowBroken = true; allowUnfree = true;};
     nixpkgs = import ./nixpkgs.nix { inherit config; };
-    unstable = import ./unstable.nix { inherit config; };
+    #unstable = import ./unstable.nix { inherit config; };
     normalHaskPkgs = nixpkgs.haskellPackages;
-    overlays = [(import ./gRPC-haskell/release.nix).overlay ];
+    #overlays = [(import ./gRPC-haskell/release.nix).overlay ];
     awakePkgs = import ./nixpkgs.nix { inherit config overlays; };
     grpcPkg = awakePkgs;
     grakn-haskell = grpcPkg.haskellPackages.callPackage ./default.nix { };
+    grpc_awake_src=fetchGit {
+                    url="https://github.com/awakesecurity/gRPC-haskell";
+                    
+                };
+    overlays= [(import "${grpc_awake_src}/release.nix").overlay ];
 in {
     build = grpcPkg.usesGRPC grakn-haskell;
 }
