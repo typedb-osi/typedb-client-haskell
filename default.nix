3ed49c8319da083c9544a7035704f566f42f28e6
faebl <faebl.taylor@protonmail.com>
2021-06-30 13:23:35 +0200

    worked on nix-bild; not finished yet; added architecture diagrams

--- default.nix
+++ default.nix
@@ -1,12 +1,14 @@
-let typedb_client_env = (import ./release.nix).build.env;
-    cabal = (import ./nixpkgs.nix{}).haskellPackages.Cabal_3_2_0_0;
+let typedb_client_env = (import ./release.nix).build;
+    cabal = (import ./nixpkgs.nix{}).haskellPackages.Cabal;
 in
 with import <nixpkgs>{};
 stdenv.mkDerivation{
   name = "typedb-client";
   version = "0.0.0.1";
   src = ./.;
+  buildInputs = [typedb_client_env ];
   buildPhase = ''
-        ${cabal}/bin/cabal update && ${cabal}/bin/cabal build
+        echo "hi"
+        cabal update && cabal build
       '';
 }
